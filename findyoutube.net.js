c1f2f05e4b3557188d54c25791b892c6be8b8158
FindHao <find@findhao.net>
2017-10-08 15:50:31 +0800

    修复新界面下下载按钮不存在的问题

--- findyoutube.net.js
+++ findyoutube.net.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name      Download Youtube videos and subtitles
 // @namespace  https://www.findhao.net
-// @version    0.2.9
+// @version    0.3.3
 // @description  获取youtube视频和字幕的下载链接
 // @include http://www.youtube.com/*
 // @include https://www.youtube.com/*
@@ -21,75 +21,51 @@
 // @copyright  2017+, Find
 // @author FindHao
 // ==/UserScript==
-$(document).ready(function () {
+function go() {
+    start();
+    removeframe();
+}
 
-    var BUTTON_TEXT = {
-        'ar': 'تنزيل',
-        'cs': 'Stáhnout',
-        'de': 'Herunterladen',
-        'en': 'Download',
-        'es': 'Descargar',
-        'fr': 'Télécharger',
-        'hi': 'डाउनलोड',
-        'hu': 'Letöltés',
-        'id': 'Unduh',
-        'it': 'Scarica',
-        'ja': 'ダウンロード',
-        'ko': '내려받기',
-        'pl': 'Pobierz',
-        'pt': 'Baixar',
-        'ro': 'Descărcați',
-        'ru': 'Скачать',
-        'tr': 'İndir',
-        'zh': '下载',
-        'zh-TW': '下載'
-    };
-    var BUTTON_TOOLTIP = {
-        'ar': 'تنزيل هذا الفيديو',
-        'cs': 'Stáhnout toto video',
-        'de': 'Dieses Video herunterladen',
-        'en': 'Download this video',
-        'es': 'Descargar este vídeo',
-        'fr': 'Télécharger cette vidéo',
-        'hi': 'वीडियो डाउनलोड करें',
-        'hu': 'Videó letöltése',
-        'id': 'Unduh video ini',
-        'it': 'Scarica questo video',
-        'ja': 'このビデオをダウンロードする',
-        'ko': '이 비디오를 내려받기',
-        'pl': 'Pobierz plik wideo',
-        'pt': 'Baixar este vídeo',
-        'ro': 'Descărcați acest videoclip',
-        'ru': 'Скачать это видео',
-        'tr': 'Bu videoyu indir',
-        'zh': '下载此视频',
-        'zh-TW': '下載此影片'
-    };
-    var RANDOM = 7489235179; // Math.floor(Math.random()*1234567890);
-    var CONTAINER_ID = 'download-youtube-video' + RANDOM;
+window.addEventListener('spfdone', go, false);
+window.addEventListener('DOMContentLoaded', go, false);
+window.addEventListener('yt-navigate-finish', go, false);
 
+function start() {
     function isMaterial() {
         var temp;
         temp = document.querySelector("ytd-app, [src*='polymer'],link[href*='polymer']");
-        if (temp && !document.getElementById("material-notice")) {
+        if (!temp) { // old UI
+            temp = document.createElement("template");
+            temp.innerHTML = //
+                `<div id='material-notice' style='border-radius:2px;color:#FFF;padding:10px;background-color:#ff0000;box-shadow:0 0 3px rgba(0,0,0,.5);font-size:18px;position:fixed;bottom:20px;right:50px;z-index:99999'>
+				<strong><ins>WARNING : </ins></strong>FindYoutbe.net script is <B>Only compatible with the new YouTube Material Layout </B> 脚本只能在新界面下使用。请切换到新的界面<br>
+				<a href='https://youtube.com/new' target='_blank' style='font-weight:bold;'>Click here</a> to activate the new YouTube Material Layout.<br>
+				<br/><br/>
+				<span id='close' onclick='document.getElementById("material-notice").remove(); return false;' align='center' STYLE='display:block;width:100px;height: 100%;margin: 0 auto;'><strong><ins><a href=""> [X] CLOSE </a></ins></strong></span>
+				</div>`;
+            document.documentElement.appendChild(temp.content.firstChild);
+            document.documentElement.removeAttribute("data-user_settings");
             return true;
         }
-        return false;
+    }
+    isMaterial();
+    var lasturl = "";
+
+    function check() {
+        if (location.href == lasturl) return;
+        lasturl = location.href;
+        if (lasturl.indexOf("watch?v=")) removeframe();
+    }
+    setInterval(check, 1000);
+
+    function removeframe() {
+        var frm_div = document.getElementById('EXT_DIV');
+        if (frm_div) {
+            frm_div.parentElement.removeChild(frm_div);
+        }
     }
 
-    function init() {
-        unsafeWindow.caption_array = [];
-        inject_our_script();
-        first_load = false;
-
-    }
-
-    function inject_our_script() {
-        var div_www_findyoutube_net = document.createElement('div'),
-            buttonElement = document.createElement('button');
-        controls = document.getElementById('watch7-headline');  // Youtube video title DIV
-        div_www_findyoutube_net.setAttribute('style', "display: table;margin-top:4px;border: 1px solid rgb(0, 183, 90);cursor: pointer; color: rgb(255, 255, 255);             border-top-left-radius: 3px;border-top-right-radius: 3px;border-bottom-right-radius: 3px; border-bottom-left-radius: 3px; background-color: #00B75A;padding: 4px;padding-right: 8px;");
-
+    bvd2_btn_onclick = function() {
         var form1 = document.createElement("form");
         form1.id = "post";
         form1.name = "post";
@@ -102,169 +78,90 @@ $(document).ready(function () {
         input.value = window.location.href;
         document.body.appendChild(form1);
         form1.appendChild(input);
-        buttonElement.id = "www-findyoutube-net-button";
-        buttonElement.className = "style-scope ytd-button-renderer style-default";
-        buttonElement.innerHTML = "Download video and subtitles";
-        buttonElement.setAttribute('style', "width:300px;height:30px;");
-        div_www_findyoutube_net.id = 'youtube-downloader-by-findyoutube-net';
-        buttonElement.addEventListener('click', function () {
-            form1.submit();
-        }, false);
+        form1.submit();
+    };
 
-        div_www_findyoutube_net.appendChild(buttonElement);
-        // put <select> into <div>
+    getSpan = function(text, className) {
+        var _tn = document.createTextNode(text);
+        var span = document.createElement("span");
+        span.className = className;
+        span.appendChild(_tn);
+        return span;
+    };
 
-        // put the div into page: new material design
-        var title_element = document.querySelectorAll('.title.style-scope.ytd-video-primary-info-renderer');
-        if (title_element) {
-            $(title_element[0]).after(div_www_findyoutube_net);
-        }
-        // put the div into page: old version
-        if (controls) {
-            controls.appendChild(div_www_findyoutube_net);
-        }
+    createButton = function() {
+        var obj = document.querySelector('#top-row>#subscribe-button');
+        if (obj !== null) {
+            // check if the button has already been created
+            var btnRow = document.getElementById('bestvd2');
+            if (btnRow === null) {
+                var bestvd2 = document.createElement("div");
+                bestvd2.id = "bestvd2";
+                bestvd2.className = "style-scope";
 
+                var bvd2_btn = document.createElement("div");
+                bvd2_btn.className = "style-scope bvd2_btn";
 
-    }
+                bvd2_btn.style = "background-color: green; border: solid 2px green; border-radius: 2px; color: white; padding: 0px 15px; font-size: 14px; cursor:pointer; height:33px;margin-right: 7px;margin-top: 7px;line-height: 33px;font-weight: 500; display:inline-block;";
 
-    if (isMaterial()) {
+                bvd2_btn.appendChild(getSpan("Download", ""));
+                bvd2_btn.onclick = bvd2_btn_onclick;
 
-        init();
-
-    } else {
-
-
-        start();
-
-        function start() {
-            var pagecontainer = document.getElementById('page-container');
-            if (!pagecontainer) return;
-            if (/^https?:\/\/www\.youtube.com\/watch\?/.test(window.location.href)) run();
-            var isAjax = /class[\w\s"'-=]+spf\-link/.test(pagecontainer.innerHTML);
-            var logocontainer = document.getElementById('logo-container');
-            if (logocontainer && !isAjax) { // fix for blocked videos
-                isAjax = (' ' + logocontainer.className + ' ').indexOf(' spf-link ') >= 0;
-            }
-            var content = document.getElementById('content');
-            if (isAjax && content) { // Ajax UI
-                var mo = window.MutationObserver || window.WebKitMutationObserver;
-                if (typeof mo !== 'undefined') {
-                    var observer = new mo(function (mutations) {
-                        mutations.forEach(function (mutation) {
-                            if (mutation.addedNodes !== null) {
-                                for (var i = 0; i < mutation.addedNodes.length; i++) {
-                                    if (mutation.addedNodes[i].id == 'watch7-main-container') { // || id=='watch7-container'
-                                        run();
-                                        break;
-                                    }
-                                }
-                            }
-                        });
-                    });
-                    observer.observe(content, {childList: true, subtree: true}); // old value: pagecontainer
-                } else { // MutationObserver fallback for old browsers
-                    pagecontainer.addEventListener('DOMNodeInserted', onNodeInserted, false);
-                }
+                obj.parentNode.insertBefore(bestvd2, obj);
+                bestvd2.appendChild(bvd2_btn);
             }
         }
+    };
 
-        function run() {
-            var args = null;
-            var usw = (typeof this.unsafeWindow !== 'undefined') ? this.unsafeWindow : window; // Firefox, Opera<15
-            if (usw.ytplayer && usw.ytplayer.config && usw.ytplayer.config.args) {
-                args = usw.ytplayer.config.args;
+    function addiframe(src, height) {
+        try {
+            var pegPlace = document.getElementById('watch-description');
+            if (pegPlace === null) {
+                pegPlace = document.getElementById('playnav-video-details');
+                if (pegPlace === null)
+                    pegPlace = document.getElementById('watch7-action-panels');
+                if (pegPlace === null)
+                    pegPlace = document.getElementById('watch8-secondary-actions');
             }
-            var videoID = null;
-            if (args) {
-                videoID = args['video_id'];
+            var iframe = document.getElementById('EXT_FRAME');
+            if (iframe === null) {
+                div = CreateIframeDiv(height);
+                iframe = CreateIframe(height);
+                div.appendChild(iframe);
+                ele = document.querySelector('#main>#info');
+                ele.parentNode.insertBefore(div, ele.nextSibling);
             }
-            var language = document.documentElement.getAttribute('lang');
-            // find parent container
-            var newWatchPage = false;
-            var parentElement = document.getElementById('watch7-action-buttons');
-            if (parentElement == null) {
-                parentElement = document.getElementById('watch8-secondary-actions');
-                if (parentElement == null) {
-                    console.log('DYVAM Error - No container for adding the download button. YouTube must have changed the code.');
-                    return;
-                } else {
-                    newWatchPage = true;
-                }
-            }
-            // get button labels
-            var buttonText = (BUTTON_TEXT[language]) ? BUTTON_TEXT[language] : BUTTON_TEXT['en'];
-            var buttonLabel = (BUTTON_TOOLTIP[language]) ? BUTTON_TOOLTIP[language] : BUTTON_TOOLTIP['en'];
-
-            // generate download code for regular interface
-            var mainSpan = document.createElement('span');
-
-            if (newWatchPage) {
-                var spanIcon = document.createElement('span');
-                spanIcon.setAttribute('class', 'yt-uix-button-icon-wrapper');
-                var imageIcon = document.createElement('img');
-                imageIcon.setAttribute('src', '//s.ytimg.com/yt/img/pixel-vfl3z5WfW.gif');
-                imageIcon.setAttribute('class', 'yt-uix-button-icon');
-                imageIcon.setAttribute('style', 'width:20px;height:20px;background-size:20px 20px;background-repeat:no-repeat;background-image: url(data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAABG0lEQVRYR+2W0Q3CMAxE2wkYAdiEEWADmIxuACMwCmzABpCTEmRSO7YTQX+ChECV43t2nF7GYeHPuLD+0AKwC/DnWMAp/N5qimkBuAfBdRTF/+2/AV6ZYFUxVYuicAfoHegd6B3oHfhZB+ByF+JyV8FkrAB74pqH3DU5L3iGoBURhdVODIQF4EjEkWLmmhYALOQgNIBcHHke4buhxXAAaFnaAhqbQ5QAOHHkwhZ8balkx1ICCiEBWNZ+CivdB7REHIC2ZjZK2oWklDDdB1NSdCd/Js2PqQMpSIKYVcM8kE6QCwDBNRCqOBJrW0CL8kCYxL0A1k6YxWsANAiXeC2ABOEWbwHAWrwxpzgkmA/JtIqnxTOElmPnjlkc4A3FykAhA42AxwAAAABJRU5ErkJggg==);');
-                spanIcon.appendChild(imageIcon);
-                mainSpan.appendChild(spanIcon);
-            }
-
-            var spanButton = document.createElement('span');
-            spanButton.setAttribute('class', 'yt-uix-button-content');
-            spanButton.appendChild(document.createTextNode(buttonText + ' '));
-            mainSpan.appendChild(spanButton);
-
-            if (!newWatchPage) { // old UI
-                var imgButton = document.createElement('img');
-                imgButton.setAttribute('class', 'yt-uix-button-arrow');
-                imgButton.setAttribute('src', '//s.ytimg.com/yt/img/pixel-vfl3z5WfW.gif');
-                mainSpan.appendChild(imgButton);
-            }
-
-            var listItems = document.createElement('ol');
-            listItems.setAttribute('style', 'display:none;');
-            listItems.setAttribute('class', 'yt-uix-button-menu');
-            // mainSpan.appendChild(listItems);
-            var buttonElement = document.createElement('button');
-            if (newWatchPage) {
-                buttonElement.setAttribute('class', 'yt-uix-button  yt-uix-button-size-default yt-uix-button-opacity yt-uix-tooltip');
-            } else { // old UI
-                buttonElement.setAttribute('class', 'yt-uix-button yt-uix-tooltip yt-uix-button-empty yt-uix-button-text');
-                buttonElement.setAttribute('style', 'margin-top:4px; margin-left:' + 5 + 'px;');
-            }
-            buttonElement.setAttribute('type', 'button');
-            buttonElement.setAttribute('role', 'button');
-            var form1 = document.createElement("form");
-            form1.id = "post";
-            form1.name = "post";
-            form1.method = "post";
-            form1.target = "_blank";
-            form1.action = "http://www.findyoutube.net/";
-            var input = document.createElement("input");
-            input.type = "hidden";
-            input.name = "url";
-            input.value = window.location.href;
-            document.body.appendChild(form1);
-            form1.appendChild(input);
-
-            buttonElement.addEventListener("click", function () {
-                form1.submit();
-            });
-            // buttonElement.addEventListener('click', function () { return false; }, false);
-            buttonElement.appendChild(mainSpan);
-            var containerSpan = document.createElement('span');
-            containerSpan.setAttribute('id', CONTAINER_ID);
-            containerSpan.appendChild(document.createTextNode(' '));
-            containerSpan.appendChild(buttonElement);
-            document.body.appendChild(form1);
-
-            // add the button
-            if (!newWatchPage) { // watch7
-                parentElement.appendChild(containerSpan);
-            } else { // watch8
-                parentElement.insertBefore(containerSpan, parentElement.firstChild);
-            }
+            src += '&type=Download';
+            src += '';
+            iframe.setAttribute("src", src);
+        } catch (err) {
+            console.log(err);
         }
     }
 
-});
+    function CreateIframe(height) {
+        iframe = document.createElement('iframe');
+        iframe.setAttribute("id", "EXT_FRAME");
+        iframe.setAttribute("width", "640px");
+        iframe.setAttribute("height", height);
+        iframe.setAttribute("border", "0");
+        iframe.setAttribute("scrolling", "no");
+        iframe.setAttribute("style", "border: 0 none;");
+        iframe.setAttribute("sandbox", "allow-scripts allow-popups allow-same-origin");
+        return iframe;
+    }
+
+    function CreateIframeDiv(height) {
+        var div = document.createElement('div');
+        div.setAttribute("id", "EXT_DIV");
+        div.style.width = '640px';
+        div.style.margin = '5px 0px 5px 0px';
+        div.style.padding = '0px';
+        div.style.height = '90px';
+        div.style.overflow = 'visible';
+        return div;
+    }
+    var intervalCheck = setInterval(function() {
+        createButton();
+    }, 250);
+}
